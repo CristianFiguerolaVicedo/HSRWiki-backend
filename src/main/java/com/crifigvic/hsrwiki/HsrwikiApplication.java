63a7cf6edde1aee0316881b53f1e7a19fe07bdf4
CriFigShe <crifigshe@gmail.com>
2025-12-26 00:03:42 +0100

    Project setup

---
+++ src/main/java/com/crifigvic/hsrwiki/HsrwikiApplication.java
@@ -0,0 +1,13 @@
+package com.crifigvic.hsrwiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsrwikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsrwikiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crifigvic/hsrwiki/HsrwikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crifigvic.hsrwiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsrwikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
